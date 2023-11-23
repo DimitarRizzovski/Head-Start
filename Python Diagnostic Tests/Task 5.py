307dd3d9596f159b98e334fa0e387d2afa5aa595
DimitarRizzovski <151799568+DimitarRizzovski@users.noreply.github.com>
2023-11-23 20:42:03 +1100

    Create Task 5.py
    Learned something new from Chat-GPT and applied it here

---
+++ Python Diagnostic Tests/Task 5.py
@@ -0,0 +1,6 @@
+def greet(name):
+  print(f"Hello {name}") # Got this after asking Chat-GPT how to add variables inside of a string without breaking it, I understand how this works and it is called formatting and this specific one is an f string.
+
+greet("Dimitar")
+greet("Mat")
+greet("Mr. Jeffreys")
