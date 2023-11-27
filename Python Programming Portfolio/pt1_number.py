d7c727011b66124068192d03b0896dd0759176d8
DimitarRizzovski <151799568+DimitarRizzovski@users.noreply.github.com>
2023-11-27 14:37:52 +1100

    Create pt1_number.py

---
+++ Python Programming Portfolio/pt1_number.py
@@ -0,0 +1,9 @@
+Number1 = int(input("Input your first number: "))
+Number2 = int(input("Input your second number: "))
+
+if Number1 > Number2:
+    print(Number1)
+elif Number1 < Number2:
+    print(Number2)
+else:
+    print("Both numbers are the same")
